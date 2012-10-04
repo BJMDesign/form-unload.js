c7ede3e9db6d94de82bab2262a36c6b8218148d2
Simon Wade <simon.wade@gmail.com>
2012-10-04 10:00:25 +1000

    Handled exclude option on initial load. Removed semicolons, for a laff.

--- form-unload.js
+++ form-unload.js
@@ -7,7 +7,7 @@ $.formUnload = {
 		// a selector of inputs to be excluded from the unload checks.
 		exclude: null
 	}
-};
+}
 
 /**
  * Call $('form').formUnload('stored') to indicate if form data has been saved without triggering the form `submit` event.
@@ -15,52 +15,53 @@ $.formUnload = {
  * @return {jQuery}
  */
 $.fn.formUnload = function( option ) {
-	var args = arguments;
+	var args = arguments
 	return this.each(function () {
 		var $this = $(this),
 			data = $this.data('formUnload'),
 			options = typeof option == 'object' && option
 		if (!data) $this.data('formUnload', (data = new FormUnload(this, options)))
 		if (typeof option == 'string') data[option].call(data, args[1])
-	});
+	})
 }
 
 var FormUnload = function( form, options ) {
-	this.$form = $(form);
-	this.options = $.extend({}, options, $.formUnload.defaults);
-	this.$inputs = this.$form.find(this.options.inputsSelector);
-	this.stored();
-	var self = this;
+	this.$form = $(form)
+	this.options = $.extend({}, $.formUnload.defaults, options)
+	this.$inputs = $([])
+	this._addInputs(this.$form.find(this.options.inputsSelector))
+	this.stored()
+	self = this
 	this.$form.bind('submit', function() {
-		self.stored();
-	});
+		self.stored()
+	})
 	$(window).bind('beforeunload', function() {
 		var changed = self.$inputs.is(function() {
-				if( $(this).data('formUnloadValue') != $(this).val() ) {
+				if( $(this).data('formUnloadValue') != $(this).val() )
 					return true
-				}
-			});
-		if( changed > 0 ) {
-			return self.options.message;
-		}
-	});
-};
+			})
+		if( changed > 0 )
+			return self.options.message
+	})
+}
 FormUnload.prototype = {
 	stored: function() {
-		this.$inputs.each(this.storeValue);
+		this.$inputs.each(this.storeValue)
 	},
 	storeValue: function() {
-		$(this).data('formUnloadValue', $(this).val());
+		$(this).data('formUnloadValue', $(this).val())
 	},
-	addInputs: function( $els ) {
-		var inputs = $els.find(this.options.inputsSelector)
-		if( !inputs.length ) {
-			inputs = $els.filter(this.options.inputsSelector)
-		}
-		if( this.options.exclude )
-			inputs = inputs.filter(this.options.exclude)
-		inputs.each(this.storeValue)
-		this.$inputs = this.$inputs.add(inputs)
+	addInputs: function($els) {
+		var $inputs = $els.find(this.options.inputsSelector)
+		if(!$inputs.length)
+			$inputs = $els.filter(this.options.inputsSelector)
+		this._addInputs($inputs)
+	},
+	_addInputs: function($inputs) {
+		if(this.options.exclude)
+			$inputs = $inputs.not(this.$form.find(this.options.exclude))
+		$inputs.each(this.storeValue)
+		this.$inputs = this.$inputs.add($inputs)
 	}
 };
 
